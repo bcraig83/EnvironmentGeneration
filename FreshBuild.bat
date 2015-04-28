68f8bee5aa7d02cf2270462b377561529f9c3e31
Ben Craig <bcraig83@gmail.com>
2015-04-28 23:02:42 +0100

    Added better instructions and initial build bat file

---
+++ FreshBuild.bat
@@ -0,0 +1,5 @@
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+cinst boxstarter -y
+
+START http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/bcraig83/EnvironmentGeneration/master/VanillaBuild.txt
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # EnvironmentGeneration
 A simple collection of scripts for generating a programming environment for .Net / Java / Python
 
+Instructions:
 
-Just browse to here:
-
-https://db.tt/LU3xv0A5
+1) Run FreshBuild.bat
+2) Adjust "InstallPackages.bat"; then run.
